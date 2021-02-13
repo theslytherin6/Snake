1edd08924c97f1a70fea3367c61e6a52b698e0bf
Basilio David Gomez Fernandez <basilio.david.g@gmail.com>
2021-02-13 20:09:56 +0100

    Game working

--- core/src/com/mygdx/game/Controller.java
+++ core/src/com/mygdx/game/Controller.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Controller {
@@ -9,21 +10,39 @@ public class Controller {
     private final int INIT_SNAKE_RELATIVE_COL = 10;
     private final int INIT_SNAKE_DIRECTION = Snake.RIGHT;
     private Snake snake;
+    private KeyBoardEmulator keyBoardEmulator;
+    private int displayWidth;
+    private int displayHeight;
 
 
-    private Controller(float cellWith) {
+    private Controller(int cellWith, int displayWidth, int displayHeight) {
         this.snake = new Snake(this.INIT_SNAKE_RELATIVE_COL, this.INIT_SNAKE_RELATIVE_ROW, this.INIT_SNAKE_DIRECTION, cellWith);
+        this.keyBoardEmulator = new KeyBoardEmulator(displayWidth, displayHeight);
+        this.displayWidth = displayWidth;
+        this.displayHeight = displayHeight;
     }
 
-    public static Controller create(float cellWith) {
+    public static Controller create(int cellWith, int displayWidth, int displayHeight) {
         if (Controller.controller == null)
-            Controller.controller = new Controller(cellWith);
+            Controller.controller = new Controller(cellWith, displayWidth, displayHeight);
         return Controller.controller;
     }
 
     public void render(SpriteBatch spriteBatch) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // todo estas lineas comentadas tienen que descomentarse para que todo funcione
         this.snake.render(spriteBatch);
+        boolean screenTouched = Gdx.input.justTouched();
+        if (screenTouched){
+            keyBoardEmulator.emulate(Gdx.input.getX(), Gdx.input.getY());
+            System.out.println("X: " + Gdx.input.getX() + "\nY: " + Gdx.input.getY());
+            System.out.println("U:" + keyBoardEmulator.isKeyUpPressed());
+            System.out.println("D:" + keyBoardEmulator.isKeyDownPressed());
+            System.out.println("L:" + keyBoardEmulator.isKeyLeftPressed());
+            System.out.println("R:" + keyBoardEmulator.isKeyRightPressed());
+            System.out.println("-------------------------------------");
+        }
     }
 
     private void moveSnake() {
